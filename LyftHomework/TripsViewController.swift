5a0d288028b9501d2022eefcce10322efdc79ee0
Andy Bartholomew <andy.bartholomew@gmail.com>
2015-08-06 13:08:43 -0700

    set up basic table view cells with fake data

--- LyftHomework/TripsViewController.swift
+++ LyftHomework/TripsViewController.swift
@@ -1,12 +1,41 @@
 import UIKit
 
-class TripsViewController: UIViewController {
+class TripsViewController: UIViewController, UITableViewDataSource {
 
+    let cellId = "tripCell"
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let fakeData = [
+        ("1638 3rd St > 568 Brannan St", "1:50pm-2:05pm (15min)"),
+        ("1481 3rd St > 1639 3rd St", "1:37pm-2:05pm (3min)"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.dataSource = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection: Int) -> Int {
+        assert(numberOfRowsInSection == 0)
+        return fakeData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        assert(tableView == self.tableView)
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellId, forIndexPath: indexPath) as! UITableViewCell
         
-        // Do any additional setup after loading the view, typically from a nib.
-        println("hey")
+        // Apparently need this for the separator to work in ios 8
+        cell.preservesSuperviewLayoutMargins = false
+        // TODO: it still doesn't actually work though
+        
+        let data = fakeData[indexPath.row]
+        
+        cell.textLabel?.text = data.0
+        cell.detailTextLabel?.text = data.1
+        
+        return cell
     }
 
 }
