8327bc35868e963f1fdb048f12e2150bbe7f3e11
Andy Bartholomew <andy.bartholomew@gmail.com>
2015-08-06 11:46:29 -0700

    set up table view, header, and nav bar

--- LyftHomework/TripsViewController.swift
+++ LyftHomework/TripsViewController.swift
@@ -4,6 +4,7 @@ class TripsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
         println("hey")
     }
